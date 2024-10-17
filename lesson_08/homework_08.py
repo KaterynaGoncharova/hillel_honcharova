47935628c1e39abc463390b59d2fa8ef973d38f9
KaterynaGoncharova <kgoncharova14@gmail.com>
2024-10-17 16:22:02 +0300

    Changes have been made to the homework 8.

---
+++ lesson_08/homework_08.py
@@ -0,0 +1,13 @@
+numbers = ["1,2,3,4", "1,2,3,4,50", "qwerty1,2,3"]
+def calculate_sum(string):
+    try:
+        num_list = [int(num) for num in string.split(',')]
+        return sum(num_list)
+        numbers = [num for num in numbers]
+    except ValueError:
+        return "Не можу це зробити"
+
+for num_string in numbers:
+    print(calculate_sum(num_string))
+
+
