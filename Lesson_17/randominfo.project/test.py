570460a62358361e43085ff6cb718a4329b2f6da
KaterynaGoncharova <kgoncharova14@gmail.com>
2024-11-25 18:55:07 +0200

    Changes have been made to the homework 17.

--- Lesson_17/randominfo.project/test.py
+++ Lesson_17/randominfo.project/test.py
@@ -1,17 +1,3 @@
-import random
-
-allAddrs = [
-    '123 Main St, Springfield',
-    '456 Elm St, Shelbyville',
-    '789 Oak St, Capital City'
-]
-
-def get_address():
-    return random.choice(allAddrs) if allAddrs else "Немає доступних адрес"
-
-class Person:
-    def __init__(self):
-        self.address = get_address()
-
-person = Person()
-print(person.address)
+import randominfo
+person = randominfo.Person()
+print(person.full_name, person.gender, person.country, person.address)
